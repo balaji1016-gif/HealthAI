3f26e1885b39cbf9018052a20e4b5848b4bb1f10
balaji1016-gif <bbala0320@gmail.com>
2026-04-09 02:43:30 +0530

    Update authServices.js

--- healthcare-frontend/src/services/authServices.js
+++ healthcare-frontend/src/services/authServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8081/api/auth";
+const API_URL = "https://healthai-nx8q.onrender.com/api/auth";
 
 export const registerUser = (userData) => axios.post(`${API_URL}/register`, userData);
 export const loginUser = (credentials) => axios.post(`${API_URL}/login`, credentials);
