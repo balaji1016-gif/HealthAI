6db85cafbede74ef432ee024d43b992e7380d543
balaji1016-gif <bbala0320@gmail.com>
2026-04-10 12:29:53 +0530

    Update register.jsx

--- healthcare-frontend/src/pages/register.jsx
+++ healthcare-frontend/src/pages/register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import API from '../src/api';
+import API from '../api';
 import toast from 'react-hot-toast';
 
 const Register = () => {
