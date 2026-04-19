7b9c1385b3b7716e6788f45afde6d3d1bcb81bb8
balaji1016-gif <bbala0320@gmail.com>
2026-04-19 09:55:17 +0530

    Update PatientRepository.java

--- src/main/java/com/example/demo/PatientRepository.java
+++ src/main/java/com/example/demo/PatientRepository.java
@@ -3,13 +3,9 @@ package com.example.demo;
 import com.example.demo.patient.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-// THIS IS THE MISSING LINE:
 import java.util.Optional; 
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, String> {
-    
-    // Now the compiler will recognize "Optional" here
     Optional<Patient> findByEmail(String email);
 }
